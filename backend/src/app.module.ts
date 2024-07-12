5f375725efc8c26862831224047f48f1d47cb56f
Gustavo Malvestiti <gustavo.malvestiti@vrsoft.com.br>
2024-07-12 12:15:09 -0300

    [feat] typeorm e database config
    Signed-off-by: Gustavo Malvestiti <gustavo.malvestiti@vrsoft.com.br>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './config/database/database.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AppService } from './app.service';
         }`,
       isGlobal: true,
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/config/database/database.module.ts
@@ -0,0 +1,25 @@
+﻿import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        logging: configService.get<boolean>('DB_LOGGING', false),
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE', false),
+        autoLoadEntities: configService.get<boolean>('DB_SYNCHRONIZE', false),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule { }
