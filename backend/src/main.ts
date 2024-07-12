694160d7f2a6f26006b86379d58e212c8ef963d6
Gustavo Malvestiti <gustavo.malvestiti@vrsoft.com.br>
2024-07-12 11:50:07 -0300

    [feat] adicionado swager ui
    Signed-off-by: Gustavo Malvestiti <gustavo.malvestiti@vrsoft.com.br>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,40 @@
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  setupOpenApi(app);
+
   await app.listen(3200);
+
+  Logger.log(
+    `Application is running on: ${await app.getUrl()}`,
+    'Code Talent Pool - Backend',
+  );
 }
 bootstrap();
+
+function setupOpenApi(app: INestApplication): void {
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Code Talent Pool - Backend')
+      .setDescription(
+        'Backend para o teste prático do Code Talent Pool da VR Software.',
+      )
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document, { useGlobalPrefix: true });
+
+    Logger.log(
+      `Swagger UI is running on path http://localhost:3200/docs`,
+      'Code Talent Pool - Backend',
+    );
+  }
+}
+
+// https://www.youtube.com/watch?v=jgpJVI3tDbY
